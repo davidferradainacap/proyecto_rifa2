28f52779eb82a729e83c54ca3571c759a8033059
david <daldeaferrada@gmail.com>
2025-09-08 05:37:16 -0300

    Mejoras visuales, animaciones y navbar Mario Kart

--- rifaed/settings.py
+++ rifaed/settings.py
@@ -123,16 +123,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "miapp/static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
-<<<<<<< HEAD
-=======
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
->>>>>>> b15445d848f7325f671d59eb1608baa8342da5a5
+
 # Media files (User uploads)
 # https://docs.djangoproject.com/en/4.1/topics/files/
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
