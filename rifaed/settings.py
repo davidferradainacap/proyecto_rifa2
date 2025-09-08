5c996f443b4344755438ab774f60c05a0046264c
davidferradainacap <david.ferrada@inacapmail.cl>
2025-09-08 00:18:29 +0000

    Actualización del proyecto

--- rifaed/settings.py
+++ rifaed/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "miapp/static"]
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Media files (User uploads)
 # https://docs.djangoproject.com/en/4.1/topics/files/
 
